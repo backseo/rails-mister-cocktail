97a5191d6549646fe2c005f7b2b7ecf2807a9b05
serge cleoron <sc21417@gmail.com>
2017-11-11 21:52:48 +0100

    addnavbarlink add buttonsbanner

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,9 @@
 class Cocktail < ApplicationRecord
+  mount_uploader :picture, PhotoUploader
   validates :name, presence: true, allow_nil: false
   validates :name, uniqueness: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+
+
 end
